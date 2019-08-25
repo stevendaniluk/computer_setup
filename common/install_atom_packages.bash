6038a1c6b660817329125e43b1062df45e1af3b4
stevendaniluk <stevendaniluk8@gmail.com>
2019-08-25 16:52:36 -0700

    Add file-types atom package

--- common/install_atom_packages.bash
+++ common/install_atom_packages.bash
@@ -3,6 +3,7 @@
 # Install some additional packages for the Atom editor
 echo "Installing Atom editor packages"
 apm install \
+  file-types \
   highlight-selected \
   language-cmake \
   minimap \
