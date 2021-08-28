844d01e01c7eaf20d30cf7d159baecab861ea8ff
Steven Daniluk <steven.j.daniluk@protonmail.com>
2021-08-28 11:06:16 -0700

    Don't install Atom editor by default on Ubuntu

--- ubuntu/install_packages.bash
+++ ubuntu/install_packages.bash
@@ -14,10 +14,6 @@ fi
 $SUDO wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | $SUDO apt-key add -
 $SUDO echo "deb https://download.sublimetext.com/ apt/stable/" | $SUDO tee /etc/apt/sources.list.d/sublime-text.list
 
-# Prep for Atom editor
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | $SUDO apt-key add -
-echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | $SUDO tee /etc/apt/sources.list.d/atom.list
-
 # Prep for Terminator
 # PPA taken from the install instructions on the terminator repo for Ubuntu 20.04
 $SUDO add-apt-repository ppa:mattrose/terminator
@@ -30,7 +26,6 @@ $SUDO apt upgrade -y
 # (thefuck requires python3-dev and python3-pip)
 $SUDO apt install -y \
   arduino \
-  atom \
   atop \
   can-utils \
   clang-format \
@@ -67,3 +62,8 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Add the terminator config file
 cp -r $SRC_DIR/config/terminator ~/.config/
+
+# Lines below are for optional Atom editor installation
+# wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | $SUDO apt-key add -
+# echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | $SUDO tee /etc/apt/sources.list.d/atom.list
+# $SUDO apt install -y atom
